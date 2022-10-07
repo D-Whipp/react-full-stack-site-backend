133bc8463a0e77810fc37cc48ebb2b3b301a60f9
Mr Whipp <dwhipp88@gmail.com>
2022-10-06 22:51:02 -0600

    took a look at req params

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 import express from 'express';
 
+// PUT /articles/learn-react/upvote
+
 const app = express();
 app.use(express.json());
 
 app.post('/hello', (req, res) => {
   console.log(req.body);
   res.send(`Hello ${req.body.name}`);
+});
+
+app.get('/hello/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`I'm lisening ${name}...`);
 });
 
 app.listen(8000, () => {
