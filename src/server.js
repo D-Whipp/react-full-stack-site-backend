f414b34684b3f7198c53ab110c9ebac037704b6d
David Whipple <dwhipp88@gmail.com>
2022-10-07 19:19:02 -0600

    created api/upvote endpoint

--- src/server.js
+++ src/server.js
@@ -1,19 +1,34 @@
 import express from 'express';
 
+let articlesInfo = [
+  {
+    name: 'learn-react',
+    upvotes: 0,
+  },
+  {
+    name: 'learn-node',
+    upvotes: 0,
+  },
+  {
+    name: 'mongodb',
+    upvotes: 0,
+  },
+];
+
 // PUT /articles/learn-react/upvote
 
 const app = express();
 app.use(express.json());
 
-app.post('/hello', (req, res) => {
-  console.log(req.body);
-  res.send(`Hello ${req.body.name}`);
-});
-
-app.get('/hello/:name/goodbye/:otherName', (req, res) => {
-  console.log(req.params);
-  const { name } = req.params;
-  res.send(`I'm lisening ${name}...`);
+app.put('/api/articles/:name/upvote', (req, res) => {
+  const {name} = req.params;
+  const article = articlesInfo.find(a=> a.name === name);
+  if(article){
+    article.upvotes += 1;
+    res.send(`The ${name} article now has ${article.upvotes} upvotes`)
+  } else {
+    res.send('That article doesn\'t exist')
+  }
 });
 
 app.listen(8000, () => {
