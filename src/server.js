159124580696830e24b02ff38a18592270c7e2df
David Whipple <dwhipp88@gmail.com>
2022-10-07 19:39:32 -0600

    added api/comments endpoint

--- src/server.js
+++ src/server.js
@@ -4,14 +4,17 @@ let articlesInfo = [
   {
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
   },
   {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
   },
   {
     name: 'mongodb',
     upvotes: 0,
+    comments: [],
   },
 ];
 
@@ -21,13 +24,29 @@ const app = express();
 app.use(express.json());
 
 app.put('/api/articles/:name/upvote', (req, res) => {
-  const {name} = req.params;
-  const article = articlesInfo.find(a=> a.name === name);
-  if(article){
+  const { name } = req.params;
+  const article = articlesInfo.find((a) => a.name === name);
+  if (article) {
     article.upvotes += 1;
-    res.send(`The ${name} article now has ${article.upvotes} upvotes.`)
+    res.send(
+      `The ${name} article now has ${article.upvotes} upvotes.`
+    );
   } else {
-    res.send('That article doesn\'t exist.')
+    res.send("That article doesn't exist.");
+  }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articlesInfo.find((a) => a.name === name);
+
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
+  } else {
+    res.send('Article does not exist.');
   }
 });
 
