4da3fb82c612d1e228692b52d7d2a1be6266a056
David Whipple <dwhipp88@gmail.com>
2022-10-20 16:31:54 -0600

    setup firebase auth to backend

--- src/server.js
+++ src/server.js
@@ -1,18 +1,47 @@
 import express from 'express';
 import { db, connectToDb } from './db.js';
+import fs from 'fs';
+import admin from 'firebase-admin';
+
+const credentials = JSON.parse(
+  fs.readFileSync('../credentials.json')
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(credentials),
+});
+
 // PUT /articles/learn-react/upvote
 
 const app = express();
 app.use(express.json());
 
+app.use(async (req, res, next) => {
+  const { authtoken } = req.headers;
+
+  if (authtoken) {
+    try {
+      const user = await admin.auth().verifyIdToken(authtoken);
+      req.user = user;
+    } catch (e) {
+      res.sendStatus(400);
+    }
+  }
+
+  next();
+});
+
 app.get('/api/articles/:name', async (req, res) => {
   const { name } = req.params;
+  const { uid } = req.user;
 
   const article = await db
     .collection('articles')
     .findOne({ name: name });
 
   if (article) {
+    const upvoteIds = article.upvoteIds || [];
+    article.canUpvote = uid && !upvoteIds.include(uid);
     res.json(article);
   } else {
     res.sendStatus(404);
